7f62da124e0e721ad96d885e707dc12cf8292f05
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2025-10-09 21:40:14 +0530

    Time: 4 ms (100.00%) | Memory: 44.4 MB (26.65%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num = 0;
+        int temp = x;
+        if (x < 0) return false;
+        while(x > 0){
+            num = num * 10 + (x % 10);
+            x = x / 10;
+        }
+        return temp == num;
+    }
+}
